45b6bbef4963d88d5824904bd0a573ec9ca3ee62
Ram-20062003 <ramajs2003@gmail.com>
2022-04-02 16:28:56 +0530

    remove: delete all 2021 files

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:aaveg_app/pages/homepage.dart';
-import 'package:aaveg_app/pages/loginpage.dart';
-import 'package:aaveg_app/services/notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:aaveg_app/pages/splashpage.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,32 +17,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  PushNotifcation? firebase;
-
-  handleAsync() async {
-    await firebase!.initialize(context);
-
-    String? token = await firebase!.getToken();
-    await firebase!.subscribeToTopic('aaveg');
-    print('Firebase token : $token');
-  }
 
   @override
   void initState() {
     super.initState();
-    firebase = PushNotifcation();
-    handleAsync();
   }
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Aaveg',
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/splash',
-        routes: {
-          '/splash': (context) => const SplashPage(),
-          '/login': (context) => LoginPage(),
-          '/home': (context) => HomePage()
-        },
+        
       );
 }
--- lib/models/leaderboard_wrap.dart
+++
@@ -1,8 +0,0 @@
-import 'package:aaveg_app/models/game_response.dart';
-
-class LeaderBoardWrap {
-  String rank;
-  Game gameResponse;
-
-  LeaderBoardWrap(this.rank, this.gameResponse);
-}
