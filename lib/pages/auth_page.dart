ffad6f2c8da6454d1390fd7b84f30d7e9c8790e1
Ram-20062003 <ramajs2003@gmail.com>
2022-05-28 01:10:07 +0530

    feat(logger): added logger for efficient logging

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -3,6 +3,8 @@ import 'package:aaveg_app/views/widgets/dauth_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../utils/logger.dart';
+
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
 
@@ -13,10 +15,11 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
+    final log = logger(AuthPage);
     final storage = Get.find<StorageService>();
-    print("Home ${storage.getJwt()}");
+    log.i("Home ${storage.getJwt()}");
     if (storage.getJwt() != null) {
-      print("Success");
+      log.e("Success");
     }
     return Scaffold(
       body: Center(child: DauthWidget()),
