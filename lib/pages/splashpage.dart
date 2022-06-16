1e7a0735f747defd56181c1b442c1375e954c388
avvsrohith <avvsrohith@gmail.com>
2022-06-16 17:57:10 +0530

    fix: remove unused import

--- lib/pages/splashpage.dart
+++ lib/pages/splashpage.dart
@@ -1,5 +1,4 @@
 import 'package:aaveg_app/pages/auth_page.dart';
-import 'package:aaveg_app/pages/home.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
